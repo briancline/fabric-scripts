d3cb1a4529b53d15cd89c68c6cce68d34ac0178c
Brian Cline <brian.cline@gmail.com>
2013-05-18 20:48:08 -0500

    Cleanup and fixes to build box tasks, and util module
    - Move sysctl and file_update to util module
    - Extend file_update to include a prefix param to look for matching
      lines to modify, or append if not found
    - Fix ntpd package name to ntp (for EL6)
    - Fix issue with push_key due to unreleased bugfix in
      Fabric.contrib.files, which didn't deal well with filenames containing
      a tilde

--- package-builder/fabfile.py
+++ package-builder/fabfile.py
@@ -1,33 +1,23 @@
-from os.path import exists
+from os.path import exists, expanduser
 from fabric.api import run, task, warn_only
 from fabric.contrib import files
 from fabric.colors import red
-from util import yum, service
-
-
-def file_update(file_name, text, sudo=False, partial=True, escape=True,
-                shell=False):
-    files.append(file_name, text, use_sudo=sudo,
-                 partial=partial, escape=escape, shell=shell)
-
-
-def sysctl(setting, value=None):
-    option = '%s=%s' % (setting, value)
-    file_update('/etc/sysctl.conf', option)
-    run('sysctl -p')
+from util import yum, service, sysctl, file_update
 
 
 @task
 def push_key():
-    if not exists('~/.ssh/id_rsa.pub'):
-        print(red('No id_rsa.pub key found.'))
+    key_path = expanduser('~/.ssh/id_rsa.pub')
+    if not exists(key_path):
+        print(red('%s not found.' % key_path))
         return
 
-    key = open('~/.ssh/id_rsa.pub').read()
+    key = open(key_path).read().strip()
     with warn_only():
-        run("mkdir ~/.ssh")
-        run("chmod 700 ~/.ssh")
-        files.append('~/.ssh/authorized_keys', key)
+        run('mkdir -p ~/.ssh && chmod 700 ~/.ssh')
+        # TODO: $HOME used with append due to a fabric bugfix pending release
+        #       Should be available in 1.6.1 or 1.7.0, whichever is next.
+        files.append('$HOME/.ssh/authorized_keys', key, partial=True)
 
 
 @task
@@ -37,15 +27,11 @@ def server_install():
         sysctl('vm.swappiness', 0)
 
     # Basic system configuration
-    file_update('/etc/sysconfig/clock', 'ZONE="America/Chicago"')
+    file_update('/etc/sysconfig/clock', 'ZONE=', 'ZONE="America/Chicago"')
     run('cp -f /usr/share/zoneinfo/America/Chicago /etc/localtime')
 
-    # Perform updates and installs
-    #apt.configure()
-    #apt.install('python-software-properties', 'software-properties-common')
-
     yum.update()
-    yum.remove('cups', 'qt', 'httpd', 'java-1.5.0-gcj')
+    yum.install('ntp')
 
     with warn_only():
         service.stop('ntpd')
--- package-builder/util/__init__.py
+++ package-builder/util/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from fabric.api import run
+from fabric.contrib import files
 
 
 class ConfigFile(object):
@@ -14,3 +15,21 @@ class ConfigFile(object):
 
     def sed(self, pattern):
         run("sed -e '%s' -i %s" % (pattern, self.file_name))
+
+
+def file_update(file_name, prefix, text, use_sudo=False):
+    prefix = '^%s' % prefix
+    if files.contains(file_name, prefix, escape=False):
+        prefix = '%s.*$' % prefix
+        files.sed(file_name, prefix, text)
+    else:
+        files.append(file_name, text, use_sudo=use_sudo)
+
+
+def sysctl(setting, value=None, reload=True):
+    seek = '%s\s*=' % setting
+    option = '%s=%s' % (setting, value)
+    file_update('/etc/sysctl.conf', seek, option, use_sudo=True)
+
+    if reload:
+        run('sysctl -p')
