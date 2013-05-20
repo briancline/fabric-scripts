8062d2b9ac2ab3cea31c928c12e3302ab65a6d9f
Brian Cline <brian.cline@gmail.com>
2013-05-20 02:23:16 -0500

    Add local_sync and publish tasks for rpm.sys.fm

--- package-builder-centos.py
+++ package-builder-centos.py
@@ -1,10 +1,19 @@
 from __future__ import print_function
-from fabric.api import env, run, sudo, task, warn_only, settings
+from fabric.api import env, run, sudo, task, warn_only, settings, get, put
 from fabric.colors import green, blue
 from util import yum, service, sysctl, file_update, keys
+from os.path import expanduser, exists
+from os import environ
 
 env.user = 'root'
+env.use_ssh_config = True
+
 build_user = 'build'
+publish_user = environ.get('USER')
+
+local_root = expanduser('~/sysfm-sync')
+publish_root = '/www/fm.sys.rpm/pub/centos/6'
+
 sysfm_repo = 'git://github.com/briancline/sysfm-specs.git'
 
 
@@ -60,3 +69,34 @@ def bootstrap():
 
     # Huzzah!
     print(green('CentOS build instance ready!'))
+
+
+@task
+def local_sync():
+    remote_paths = ['rpmbuild/RPMS',
+                    'rpmbuild/SRPMS',
+                    'sysfm-specs']
+
+    print(blue('Syncing local copy into %s...' % local_root))
+    for remote_path in remote_paths:
+        with settings(user=build_user):
+            get(remote_path, local_root)
+
+
+@task
+def publish():
+    arch_list = ['noarch', 'i386', 'x86_64', 'updates']
+    remote_root = '/www/fm.sys.rpm/pub/centos/6'
+    local_srpms = '%s/SRPMS' % local_root
+    local_rpms = '%s/RPMS' % local_root
+
+    with settings(user=publish_user):
+        run('mkdir -p %s' % remote_root)
+        put(local_srpms, remote_root)
+
+        for arch in arch_list:
+            local_arch_root = '%s/%s' % (local_rpms, arch)
+            if not exists(local_arch_root):
+                continue
+
+            put(local_arch_root, remote_root)
